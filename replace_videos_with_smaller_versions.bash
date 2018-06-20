10185f14d9af8f0f5a25e4bc73b851f1e2208b97
Benjamin Bergman <benjamin.bergman@humanrights.ca>
2018-06-20 14:50:03 -0500

    Refactored to be suitable for files with spaces in name

--- replace_videos_with_smaller_versions.bash
+++ replace_videos_with_smaller_versions.bash
@@ -1,4 +1,6 @@
-for file in $(ls *.mp4); do
-    newfile=$(echo $file | sed 's/\.mp4/small.mp4/');
-    ffmpeg -i $file -preset veryslow -crf 30 $newfile && mv -f $newfile $file || rm -f $newfile;
-done
+find . -type f -name '*.mp4' -exec sh -c '
+    for file do
+        newfile=$(echo "$file" | sed 's/\.mp4/small.mp4/');
+        ffmpeg -loglevel warning -i "$file" -preset veryslow -crf 30 "$newfile" && mv -f "$newfile" "$file" || rm -f "$newfile";
+    done
+' sh {} +
