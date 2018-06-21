762f59afbc3446dc3df973913b5378fdb5cc549f
Benjamin Bergman <benjamin.bergman@humanrights.ca>
2018-06-21 15:23:10 -0500

    Now with human readable numbers!

--- replace_videos_with_smaller_versions.bash
+++ replace_videos_with_smaller_versions.bash
@@ -7,6 +7,6 @@ find . -type f -name '*.mp4' -exec sh -c '
         newfile=$(echo "$file" | sed 's/\.mp4/small.mp4/');
         ffmpeg -loglevel warning -i "$file" -preset veryslow -crf 30 "$newfile" && mv -f "$newfile" "$file" || rm -f "$newfile";
         new_size=$(stat -f%z "$file")
-        echo "before: $orig_size  after: $new_size  saved: $(expr $orig_size - $new_size)"
+        echo "before: $(gnumfmt --to=si $orig_size)  after: $(gnumfmt --to=si $new_size)  saved: $(gnumfmt --to=si $(expr $orig_size - $new_size)) ($(expr $(expr $(expr $orig_size - $new_size) \* 100) / $orig_size)%)"
     done
 ' sh {} +
