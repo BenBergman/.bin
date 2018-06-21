06b628582751193b229297819ab5d2257be5d748
Benjamin Bergman <benjamin.bergman@humanrights.ca>
2018-06-21 14:46:17 -0500

    Add stats on amount of space saved

--- replace_videos_with_smaller_versions.bash
+++ replace_videos_with_smaller_versions.bash
@@ -1,6 +1,12 @@
+#!/bin/bash
+
 find . -type f -name '*.mp4' -exec sh -c '
     for file do
+        printf "Processing $file   "
+        orig_size=$(stat -f%z "$file")
         newfile=$(echo "$file" | sed 's/\.mp4/small.mp4/');
         ffmpeg -loglevel warning -i "$file" -preset veryslow -crf 30 "$newfile" && mv -f "$newfile" "$file" || rm -f "$newfile";
+        new_size=$(stat -f%z "$file")
+        echo "before: $orig_size  after: $new_size  saved: $(expr $orig_size - $new_size)"
     done
 ' sh {} +
